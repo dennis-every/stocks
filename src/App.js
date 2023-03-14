14aaf129eca3b021ac808b16ccd8418144ad4269
Dennis Every <dennis@ansyra.com>
2023-03-14 07:47:00 -0400

    Add react-router, pages, navbar, layout, and navlinks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Show from './pages/Show';
+import About from './pages/About';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="show" element={<Show />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/NoMatch.jsx
@@ -0,0 +1,3 @@
+const NoMatch = () => <h1>Page not found!</h1>;
+
+export default NoMatch;
