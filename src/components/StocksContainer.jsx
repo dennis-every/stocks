b1c694723e262b373448c18ac4f21117d254328e
Dennis Every <dennis@ansyra.com>
2023-03-14 11:13:03 -0400

    fetch data in parent container and display in child component

--- src/components/StocksContainer.jsx
+++ src/components/StocksContainer.jsx
@@ -1,15 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchStocks } from '../redux/stocks/stocksSlice';
 import Stock from './Stock';
 
 const StocksContainer = () => {
-  const stocks = [{ ticker: 'AAPL' }, { ticker: 'AMZN' }, { ticker: 'TSLA' }];
+  const dispatch = useDispatch();
+  const ifSucceed = useSelector((store) => store.stocks.ifSucceed);
+  const stocksArray = useSelector((store) => store.stocks.stocksArray);
+
+  useEffect(() => {
+    dispatch(fetchStocks());
+  }, [dispatch, ifSucceed]);
 
   return (
     <>
       <h3>LIST</h3>
-      {stocks.map((stock) => (
+      {stocksArray.map((stock) => (
         <Stock
-          key={stock.ticker}
-          ticker={stock.ticker}
+          key={stock.symbol}
+          companyName={stock.companyName}
         />
       ))}
     </>
