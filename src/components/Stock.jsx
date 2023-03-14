cb3154f58fb8d49fb3694c78de39ad2afa10841a
Dennis Every <dennis@ansyra.com>
2023-03-14 14:09:56 -0400

    Display relevant data on Stock page

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,16 +1,45 @@
 import PropTypes from 'prop-types';
 
 const Stock = (props) => {
-  const { companyName } = props;
+  const {
+    symbol,
+    companyName,
+    sector,
+    marketCap,
+    beta,
+    price,
+    exchangeShortName,
+    volume,
+    lastAnnualDividend,
+  } = props;
+
   return (
     <>
-      <h3>{companyName}</h3>
+      <h3>{`Name: ${companyName}`}</h3>
+      <ul>
+        <li>{`Symbol: ${symbol}`}</li>
+        <li>{`Market cap: ${marketCap}`}</li>
+        <li>{`Sector: ${sector}`}</li>
+        <li>{`Beta: ${beta}`}</li>
+        <li>{`Price: ${price}`}</li>
+        <li>{`Exchange: ${exchangeShortName}`}</li>
+        <li>{`Volume: ${volume}`}</li>
+        <li>{`Last annual dividend: ${lastAnnualDividend}`}</li>
+      </ul>
     </>
   );
 };
 
 Stock.propTypes = {
+  symbol: PropTypes.string.isRequired,
   companyName: PropTypes.string.isRequired,
+  marketCap: PropTypes.string.isRequired,
+  sector: PropTypes.string.isRequired,
+  beta: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  lastAnnualDividend: PropTypes.string.isRequired,
+  volume: PropTypes.string.isRequired,
+  exchangeShortName: PropTypes.string.isRequired,
 };
 
 export default Stock;
--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -1,12 +1,36 @@
-import { useParams, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import StockComponent from '../components/Stock';
 
 const Stock = () => {
   const { slug } = useParams();
+  const stocksArray = useSelector((store) => store.stocks.stocksArray);
+  const content = stocksArray.find((stock) => stock.symbol === slug);
+  const {
+    symbol,
+    companyName,
+    sector,
+    marketCap,
+    beta,
+    price,
+    exchangeShortName,
+    volume,
+    lastAnnualDividend,
+  } = content;
 
   return (
     <>
-      <h1>{slug}</h1>
-      <Outlet />
+      <StockComponent
+        symbol={symbol}
+        companyName={companyName}
+        sector={sector}
+        marketCap={marketCap}
+        beta={beta}
+        price={price}
+        exchangeShortName={exchangeShortName}
+        volume={volume}
+        lastAnnualDividend={lastAnnualDividend}
+      />
     </>
   );
 };
