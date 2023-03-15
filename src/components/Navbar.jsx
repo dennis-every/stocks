1ab05ba795d521844273700f63f2b35913f86689
Dennis Every <dennis@ansyra.com>
2023-03-15 09:16:02 -0400

    Add back button to Stock show page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import './Navbar.scss';
-import { BsPatchQuestionFill, BsFillHouseDoorFill } from 'react-icons/bs';
+import {
+  BsPatchQuestionFill,
+  BsFillHouseDoorFill,
+  BsChevronLeft,
+} from 'react-icons/bs';
 
 const Navbar = () => {
-  const links = [
-    { path: '/', text: <BsFillHouseDoorFill />, key: 1 },
-    { path: '/', text: 'Market Cap', key: 2 },
-    { path: 'about', text: <BsPatchQuestionFill />, key: 3 },
-  ];
+  let links = [];
+  if (Object.keys(useParams()).length === 0) {
+    links = [
+      { path: '/', text: <BsFillHouseDoorFill />, key: 1 },
+      { path: '/', text: 'Market Cap', key: 2 },
+      { path: 'about', text: <BsPatchQuestionFill />, key: 3 },
+    ];
+  } else {
+    links = [
+      { path: '/', text: <BsChevronLeft />, key: 1 },
+      { path: '/', text: 'Company', key: 2 },
+      { path: 'about', text: <BsPatchQuestionFill />, key: 3 },
+    ];
+  }
 
   return (
     <nav>
