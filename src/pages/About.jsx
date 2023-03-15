1a60176e134160ae83e01008bf0da1f5d8be1ced
Dennis Every <dennis@ansyra.com>
2023-03-15 00:17:05 -0400

    Add styles About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,32 @@
-const About = () => <h1>About</h1>;
+import './About.scss';
+
+const About = () => (
+  <section className="about">
+    <p>
+      Welcome to our stock market research web application! Our platform is
+      designed to provide you with comprehensive market analysis to help you
+      make informed decisions about your investments.
+    </p>
+    <p>
+      Whether you&apos;re a seasoned researcher or just starting out, our
+      user-friendly interface makes it easy to manage your portfolio and monitor
+      your progress. We offer a range of tools and resources to help you make
+      sense of the data, including detailed charts and graphs, customizable
+      watchlists, and alerts to keep you informed of any significant changes in
+      the market.
+    </p>
+    <p>
+      In addition, we prioritize security and privacy, so you can research with
+      confidence knowing that your information is protected. Our team of experts
+      is dedicated to providing you with exceptional customer support and
+      ensuring that your experience on our platform is both enjoyable and
+      informative.
+    </p>
+    <p>
+      Thank you for choosing our stock market research web application. We look
+      forward to helping you achieve your financial research goals.
+    </p>
+  </section>
+);
 
 export default About;
